70a22502b9aadaa39de8e405a12e95f1cab94858
Matthew Mallory <mmallory56@gmail.com>
2022-05-20 13:10:49 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `medinalake`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+};
