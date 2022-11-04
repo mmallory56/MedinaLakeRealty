77b6ee9a38d9f889a1d94d952649686265aa11af
Matthew Mallory <40873286+mmallory56@users.noreply.github.com>
2022-11-04 11:28:51 -0500

    logo bar changes

--- src/components/LogoBar.jsx
+++ src/components/LogoBar.jsx
@@ -9,6 +9,7 @@ const Bar = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: row;
     background-color: #505050;
     border-radius: .5rem;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -17,21 +18,23 @@ const Bar = styled.div`
 
 const Item = styled(Link)`
   width:auto;
-  margin: auto;
+  
+  margin-right: 25px;
   color:white;
   text-decoration: none;
   font-size: larger;
   @media screen and (max-width: 400px) {
   
     font-size: smaller;
-  
+  margin-right:5px;
   
 }
 `
 const logoStyle={
   boxShadow:"2px 2px 5px black",
   borderRadius:"9px",
-  marginLeft:"10px"
+  marginRight:"10px"
+  
 }
 const LogoBar = () => {
   return (
