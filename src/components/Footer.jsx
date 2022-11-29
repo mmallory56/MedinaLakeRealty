d978839c0b9e7e88fa996d0a73a225abb7d848cd
Matthew Mallory <40873286+mmallory56@users.noreply.github.com>
2022-11-29 13:56:58 -0600

    added changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,16 @@ import { Link } from 'gatsby';
 import React from 'react'
 import styled from 'styled-components'
 const FooterContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-
+  display: grid;
+  grid-template-columns: repeat(auto-fit,minmax(250px,350px));
+  justify-content: center;
+  align-content: center;
   font-size: 1.5em;
-  padding-top:90px;
+  padding-top:450px;
+  padding-bottom: 250px;
+  gap:10px;
   background-image: url("/footerBackground.svg");
-  
+  background-color: #02a0a5;
   background-repeat: no-repeat;
   background-size: cover;
   width: 100%;
@@ -18,71 +21,77 @@ const FooterContainer = styled.div`
 }
   @media screen and (max-width: 900px) {
      font-size: 1.0em;
-     padding-top:70px;
+  
 }
   @media screen and (max-width: 600px) {
      font-size: .9em;
-     padding-top:30px;
+   
     
 }
   @media screen and (max-width: 400px) {
     font-size:.8 em;
-    padding-top:20px;
+   
     
 }
 @media screen and (max-width: 300px) {
   font-size:.5em;
-  padding-top:0px;
+ 
 }
 `
 const Section = styled.div`
 display: flex;
 align-items: center;
-margin-bottom: 150px;
+padding: 10px;
+
+
+text-align: center;
 flex-direction: column;
-    width:33%;
+    width:auto;
     height:auto;
-    padding-top: 600px;
+   
+   
     
-    color:#ffff;
+    color:#e6e6e6;
+    border-radius: 20px;
+background-color: #0000002d;
 `;
 const Item = styled(Link)`
-margin-top:20px;
+
 text-align: center;
-color:white;
+color:#e2e2e2d4;
 font-size: large;
 text-decoration-line: none;
 -moz-text-decoration-line:none;
-text-decoration: none !important;
+
+
 `;
 const Footer = () => {
   return (
     <FooterContainer>
         <Section>
-          
+            <h3>Listing and other Info</h3>
             <Item to="/About">About</Item>
           
              <Item to={"/Listing"}>Listings</Item> 
          
-            <Item href={"https://www.trec.texas.gov/sites/default/files/pdf-forms/IABS%201-0.pdf"}>TREC Information about brokerage services</Item>
-          
+            
             
         </Section>
         <Section>
+        <h3>Medina Lake Info</h3>
         <Item 
         href="https://www.edwardsaquifer.net/medina.html#:~:text=Medina%20Lake%20was%20constructed%20between,Antonio%2C%20just%20outside%20Loop%201604.">
           History of Medina Lake
           </Item>
-          <Item href="https://www.facebook.com/MedinaLakeRealty/">Our Facebook </Item>
-            <Item>Item</Item>
+        
            
            
         </Section>
         <Section>
-        <Item>Item</Item>
-            <Item>Item</Item>
-            <Item>Item</Item>
-            
+        <h3>Texas Real Estate Commision</h3>
+        <Item href="https://www.trec.texas.gov/">TREC Website</Item>
+        <Item href={"https://www.trec.texas.gov/sites/default/files/pdf-forms/IABS%201-0.pdf"}>TREC Information about brokerage services</Item>
+          
            
         </Section>
     </FooterContainer>
